3eafd920179c612190f1b0ad41541439dfc2cfd3
Nicolas Casas <contadorcasas@gmail.com>
2023-03-14 22:02:46 -0300

    respaldo

--- Proyecto cv1/pre1h1.js
+++ Proyecto cv1/pre1h1.js
@@ -1,4 +1,5 @@
 function myFunction() {
+    
 document.getElementById('pre1').style.display='block'
 document.getElementById('titulo1').style.display='block'
 document.getElementById('preinicio').style.display='none'
